f6585bffce9b2f7f0a9d2c7a03dc9f15300cafe0
Jarkko Kniivila <jkniiv@gmail.com>
2020-04-24 13:08:14 +0300

    Add the main DAGs (task groups/graphs) to implement a Restic-based backup regime in Apache Airflow

---
+++ dags/restic_prune.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+
+from builtins import range
+from datetime import datetime, timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+def msys2_bash_invocation(command, msys2_bash='/c/scoop/apps/msys2/current/usr/bin/bash.exe'):
+    return '{0} -l -c "{1}"'.format(msys2_bash, command)
+
+args = {
+    'owner': 'restic',
+    'depends_on_past': False,
+    #'start_date': airflow.utils.dates.days_ago(2),
+    'start_date': datetime(2020, 2, 2),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=10),
+}
+
+dag = DAG(
+    dag_id='restic_prune',
+    default_args=args,
+    schedule_interval='0 22 * * 6',
+    dagrun_timeout=timedelta(minutes=480),
+    catchup=False,
+)
+
+run_this_last = DummyOperator(
+    task_id='run_END',
+    dag=dag,
+)
+
+run_restic_prune = BashOperator(
+    task_id='run_restic_prune',
+    bash_command=msys2_bash_invocation("restic-execute-pruning-phase"),
+    dag=dag,
+)
+
+run_restic_check = BashOperator(
+    task_id='run_restic_check',
+    bash_command=msys2_bash_invocation("restic-do-check"),
+    dag=dag,
+)
+
+run_restic_prune >> run_restic_check >> run_this_last
+
+if __name__ == "__main__":
+    dag.cli()
